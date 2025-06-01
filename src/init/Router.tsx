7d19221bfbc55f9137ed4eb40751f5d3a4012451
erikh2000 <erikrhermansen@Eriks-iMac-2.local>
2025-06-01 13:37:01 -0700

    Removed test screen.

--- src/init/Router.tsx
+++ src/init/Router.tsx
@@ -1,17 +1,15 @@
 import { Route } from 'wouter';
 import LoadScreen from "@/loadScreen/LoadScreen.tsx";
 import HomeScreen from "@/homeScreen/HomeScreen.tsx";
-import TestScreen from '@/testScreen/TestScreen';
-import {LOAD_URL, HOME_URL, TEST_URL} from '@/common/urlUtil';
+import {LOAD_URL, HOME_URL} from '@/common/urlUtil';
 
 function Router() {
   return (
     <>
       <Route path={LOAD_URL} component={LoadScreen} />
       <Route path={HOME_URL} component={HomeScreen} />
-      <Route path={TEST_URL} component={TestScreen} />
     </>
   )
 }
 
-export default Router
+export default Router;
